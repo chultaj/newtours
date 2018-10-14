84444acf8d7c19a7a65657d6e14638d0b4f243ca
Pawel <pavveln@mail.ru>
2018-10-15 00:42:45 +0200

    Disable verbose logging for Chrome

--- src/main/java/com/demoaut/newtours/driver/Chrome.java
+++ src/main/java/com/demoaut/newtours/driver/Chrome.java
@@ -1,10 +1,28 @@
 package com.demoaut.newtours.driver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.logging.Level;
 
 public class Chrome extends Browser {
     protected Chrome(String driverPath) {
         System.setProperty("webdriver.chrome.driver", driverPath);
-        driver = new ChromeDriver();
+
+        LoggingPreferences pref = new LoggingPreferences();
+        pref.enable(LogType.BROWSER, Level.OFF);
+        pref.enable(LogType.DRIVER, Level.OFF);
+        pref.enable(LogType.PERFORMANCE, Level.OFF);
+
+        ChromeOptions options = new ChromeOptions();
+
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+        desiredCapabilities.setCapability(CapabilityType.LOGGING_PREFS, pref);
+
+        driver = new ChromeDriver(desiredCapabilities);
     }
 }
--- src/main/java/com/demoaut/newtours/driver/Firefox.java
+++ src/main/java/com/demoaut/newtours/driver/Firefox.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Firefox extends Browser {
     protected Firefox(String driverPath) {
         System.setProperty("webdriver.gecko.driver", driverPath);
+
         driver = new FirefoxDriver();
     }
 }
